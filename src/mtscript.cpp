9f41b487b47cbae3943f05a987516d18fb6b459d
MarcTortPascua <marctorpas@campus.monlau.com>
2024-06-03 13:26:30 +0200

    aaaaaaaaa

--- src/mtscript.cpp
+++ src/mtscript.cpp
@@ -1,8 +1,10 @@
 #include "tokens.hpp"
+#include <ios>
 #include <scanner.hpp>
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 using namespace std;
@@ -10,17 +12,9 @@ using namespace std;
 int main (int argc, char** argv){
     if (argc > 1){
         fstream archivo = fstream(argv[1]);
-        std::string filedata;
-        while (!archivo.eof()) {
-            string pre ;
-            archivo >> pre;
-            filedata += pre;
-        }
-        scaner escaneador = scaner(filedata);
-        escaneador.scan();
-        for(Token i : escaneador.getTokens()){
-            cout << i.to_string() << endl;
-        }
+        
+            cout << archivo.;
+        
     }else{
         string line = "";
         while (line != "exit") {
